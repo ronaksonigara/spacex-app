900730f87898b8eb2e1e4091c302cd653a644025
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2019-12-14 22:50:11 +0530

    Updates

---
+++ precache-manifest.d5764e3ba429452775a41335185e6732.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41a480f0082b7d90d777262f4eb27054",
+    "url": "/spacex-app/index.html"
+  },
+  {
+    "revision": "d2ec221459b8d6390518",
+    "url": "/spacex-app/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "eb89f439d5370b0a10bc",
+    "url": "/spacex-app/static/css/main.cf6f81c9.chunk.css"
+  },
+  {
+    "revision": "d2ec221459b8d6390518",
+    "url": "/spacex-app/static/js/2.98601148.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/spacex-app/static/js/2.98601148.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb89f439d5370b0a10bc",
+    "url": "/spacex-app/static/js/main.c997c02d.chunk.js"
+  },
+  {
+    "revision": "b98b16257fc375f790a1",
+    "url": "/spacex-app/static/js/runtime-main.9bc82753.js"
+  },
+  {
+    "revision": "6fb731d67c2ba54424545028ed318409",
+    "url": "/spacex-app/static/media/spacex.6fb731d6.jpeg"
+  }
+]);
